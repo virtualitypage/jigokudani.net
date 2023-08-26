fb0a1a9ee1e967c6df9841d0c4155d8ab42d296f
yuki4201 <121924587+virtualitypage@users.noreply.github.com>
2023-08-26 23:07:42 +0900

    modified

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ function information() {
   let date = new Date();
   // alert(date.toLocaleDateString() + " 現在、最新情報はありません");
   // alert(date.toLocaleDateString() + " 現在、諸事情につきサイトの更新を停止しています" + '\n' + "サイトへの率直な意見を述べられるフォームを実装しました！");
-  alert("「立呑みコクレ」「ほっこりばーる ほしぼし」のページを更新しました。");
+  alert("「立呑みコクレ」のページを更新しました。");
 }
 
 function googleForm() {
